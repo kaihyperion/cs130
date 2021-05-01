3f6990a449b04692fcd91d53599eda062e5a016a
kaihyperion <kyperion.workmode@gmail.com>
2021-04-30 23:56:18 -0500

    Checking in my hw02 folder

--- lesson9/01-style-property-demo/main.js
+++ lesson9/01-style-property-demo/main.js
@@ -1,18 +1,29 @@
 
 const makeRed = (ev) => {
     // your code here...
+    console.log('turn background red.');
+    document.querySelector('body').style.backgroundColor = 'red';
+    
 };
 
 const makeBlue = (ev) => {
     // your code here...
+    console.log('turn background blue.');
+    document.querySelector('body').style.backgroundColor = 'blue';
 };
 
 const makePink = (ev) => {
     // your code here...
+    console.log('turn background pink.');
+    document.querySelector('body').style.backgroundColor = 'yellow';
+    
 };
 
 const makeOrange = (ev) => {
     // your code here...
+    console.log('turn background orange.')
+    document.querySelector('body').style.backgroundColor = 'orange';
+    
 };
 
 
